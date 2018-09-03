a104bbd1958845e3aa42d0199ba043c2c6548171
mertsincan <sincan.mert@gmail.com>
2018-09-03 15:52:44 +0300

    Added Stacked Group Bar demo page

--- src/main/java/org/primefaces/showcase/view/chartjs/ChartJsView.java
+++ src/main/java/org/primefaces/showcase/view/chartjs/ChartJsView.java
@@ -76,6 +76,8 @@ public class ChartJsView implements Serializable {
     
     private BarChartModel stackedBarModel;
     
+    private BarChartModel stackedGroupBarModel;
+    
     private RadarChartModel radarModel;
     
     private RadarChartModel radarModel2;
@@ -96,6 +98,7 @@ public class ChartJsView implements Serializable {
         createBarModel2();
         createHorizontalBarModel();
         createStackedBarModel();
+        createStackedGroupBarModel();
         createRadarModel();
         createRadarModel2();
         createBubbleModel();
@@ -551,6 +554,89 @@ public class ChartJsView implements Serializable {
         stackedBarModel.setOptions(options);
     }
     
+    public void createStackedGroupBarModel() {
+        stackedGroupBarModel = new BarChartModel();
+        ChartData data = new ChartData();
+        
+        BarChartDataSet barDataSet = new BarChartDataSet();
+        barDataSet.setLabel("Dataset 1");
+        barDataSet.setBackgroundColor("rgb(255, 99, 132)");
+        barDataSet.setStack("Stack 0");
+        List<Number> dataVal = new ArrayList<>();
+        dataVal.add(-32);
+        dataVal.add(-70);
+        dataVal.add(-33);
+        dataVal.add(30);
+        dataVal.add(-49);
+        dataVal.add(23);
+        dataVal.add(-92);
+        barDataSet.setData(dataVal);
+        
+        BarChartDataSet barDataSet2 = new BarChartDataSet();
+        barDataSet2.setLabel("Dataset 2");
+        barDataSet2.setBackgroundColor("rgb(54, 162, 235)");
+        barDataSet2.setStack("Stack 0");
+        List<Number> dataVal2 = new ArrayList<>();
+        dataVal2.add(83);
+        dataVal2.add(18);
+        dataVal2.add(86);
+        dataVal2.add(8);
+        dataVal2.add(34);
+        dataVal2.add(46);
+        dataVal2.add(11);
+        barDataSet2.setData(dataVal2);
+        
+        BarChartDataSet barDataSet3 = new BarChartDataSet();
+        barDataSet3.setLabel("Dataset 3");
+        barDataSet3.setBackgroundColor("rgb(75, 192, 192)");
+        barDataSet3.setStack("Stack 1");
+        List<Number> dataVal3 = new ArrayList<>();
+        dataVal3.add(-45);
+        dataVal3.add(73);
+        dataVal3.add(-25);
+        dataVal3.add(65);
+        dataVal3.add(49);
+        dataVal3.add(-18);
+        dataVal3.add(46);
+        barDataSet3.setData(dataVal3);
+        
+        data.addChartDataSet(barDataSet);
+        data.addChartDataSet(barDataSet2);
+        data.addChartDataSet(barDataSet3);
+        
+        List<String> labels = new ArrayList<>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+        labels.add("July");
+        data.setLabels(labels);
+        stackedGroupBarModel.setData(data);
+        
+        //Options
+        BarChartOptions options = new BarChartOptions();
+        CartesianScales cScales = new CartesianScales();
+        CartesianLinearAxes linearAxes = new CartesianLinearAxes();
+        linearAxes.setStacked(true);    
+        cScales.addXAxesData(linearAxes);
+        cScales.addYAxesData(linearAxes);
+        options.setScales(cScales);
+        
+        Title title = new Title();
+        title.setDisplay(true);
+        title.setText("Bar Chart - Stacked Group");
+        options.setTitle(title);
+        
+        Tooltip tooltip = new Tooltip();
+        tooltip.setMode("index");
+        tooltip.setIntersect(false);
+        options.setTooltip(tooltip);  
+        
+        stackedGroupBarModel.setOptions(options);
+    }
+    
     public void createRadarModel() {
         radarModel = new RadarChartModel();
         ChartData data = new ChartData();
@@ -863,6 +949,14 @@ public class ChartJsView implements Serializable {
 
     public void setStackedBarModel(BarChartModel stackedBarModel) {
         this.stackedBarModel = stackedBarModel;
+    }
+
+    public BarChartModel getStackedGroupBarModel() {
+        return stackedGroupBarModel;
+    }
+
+    public void setStackedGroupBarModel(BarChartModel stackedGroupBarModel) {
+        this.stackedGroupBarModel = stackedGroupBarModel;
     }
 
     public RadarChartModel getRadarModel() {
