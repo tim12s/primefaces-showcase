c6b47dd35ac753fe3cb38a3c7bac0bf687a054ad
mertsincan <sincan.mert@gmail.com>
2018-10-23 10:00:30 +0300

    Refactor

--- src/main/java/org/primefaces/showcase/service/ThemeService.java
+++ src/main/java/org/primefaces/showcase/service/ThemeService.java
@@ -32,7 +32,7 @@ public class ThemeService {
     
     @PostConstruct
     public void init() {
-        themes = new ArrayList<Theme>();
+        themes = new ArrayList<>();
         themes.add(new Theme(0, "Nova-Light", "nova-light"));
         themes.add(new Theme(1, "Nova-Dark", "nova-dark"));
         themes.add(new Theme(2, "Nova-Colored", "nova-colored"));
@@ -41,7 +41,6 @@ public class ThemeService {
         themes.add(new Theme(5, "Luna-Green", "luna-green"));
         themes.add(new Theme(6, "Luna-Pink", "luna-pink"));
         themes.add(new Theme(7, "Omega", "omega"));
-        
     }
     
     public List<Theme> getThemes() {
