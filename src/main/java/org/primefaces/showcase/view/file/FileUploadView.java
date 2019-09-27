3e60c5e33734922abf63bdbf75e535dcaf6a4ca7
Thomas Andraschko <tandraschko@apache.org>
2019-09-28 01:17:05 +0200

    #5160 refatored FileUpload API

--- src/main/java/org/primefaces/showcase/view/file/FileUploadView.java
+++ src/main/java/org/primefaces/showcase/view/file/FileUploadView.java
@@ -16,39 +16,55 @@
 package org.primefaces.showcase.view.file;
 
 import org.primefaces.event.FileUploadEvent;
-import org.primefaces.model.file.UploadedFileWrapper;
-import org.primefaces.util.FileUploadUtils;
 
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import org.primefaces.model.file.UploadedFile;
+import org.primefaces.model.file.UploadedFiles;
 
 @Named
 @RequestScoped
 public class FileUploadView {
     
-    private UploadedFileWrapper file;
+    private UploadedFile file;
+    private UploadedFiles files;
 
-    public UploadedFileWrapper getFile() {
+    public UploadedFile getFile() {
         return file;
     }
 
-    public void setFile(UploadedFileWrapper file) {
+    public void setFile(UploadedFile file) {
         this.file = file;
     }
-    
+
+    public UploadedFiles getFiles() {
+        return files;
+    }
+
+    public void setFiles(UploadedFiles files) {
+        this.files = files;
+    }
+
     public void upload() {
-        if(file != null) {
-            FileUploadUtils.consume(file, f -> {
+        if (file != null) {
+            FacesMessage message = new FacesMessage("Successful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+    
+    public void uploadMultiple() {
+        if (files != null) {
+            for (UploadedFile f : files.getFiles()) {
                 FacesMessage message = new FacesMessage("Successful", f.getFileName() + " is uploaded.");
                 FacesContext.getCurrentInstance().addMessage(null, message);
-            });
+            }
         }
     }
     
     public void handleFileUpload(FileUploadEvent event) {
-		FacesMessage msg = new FacesMessage("Successful", event.getFile().getFileName() + " is uploaded.");
-		FacesContext.getCurrentInstance().addMessage(null, msg);
-	}
+        FacesMessage msg = new FacesMessage("Successful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
 }
