f6ae26f5ba22df2be3cf2faa5eeb4e2e652ae423
Jasper de Vries <jepsar@gmail.com>
2019-11-26 21:22:34 +0100

    Added globalFilterFunction example to dataTable (#91)

--- src/main/java/org/primefaces/showcase/view/data/datatable/FilterView.java
+++ src/main/java/org/primefaces/showcase/view/data/datatable/FilterView.java
@@ -15,64 +15,103 @@
  */
 package org.primefaces.showcase.view.data.datatable;
 
-import javax.faces.view.ViewScoped;
-import org.primefaces.showcase.domain.Car;
-import org.primefaces.showcase.service.CarService;
-
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-import javax.inject.Named;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Locale;
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.primefaces.showcase.domain.Car;
+import org.primefaces.showcase.service.CarService;
 
 @Named("dtFilterView")
 @ViewScoped
 public class FilterView implements Serializable {
-    
-    private List<Car> cars;
-    
-    private List<Car> filteredCars;
-    
+
+    private List<Car> cars1;
+    private List<Car> cars2;
+
+    private List<Car> filteredCars1;
+    private List<Car> filteredCars2;
+
     @Inject
     private CarService service;
 
     @PostConstruct
     public void init() {
-        cars = service.createCars(10);
+        cars1 = service.createCars(10);
+        cars2 = service.createCars(10);
     }
-    
+
     public boolean filterByPrice(Object value, Object filter, Locale locale) {
         String filterText = (filter == null) ? null : filter.toString().trim();
-        if(filterText == null||filterText.equals("")) {
+        if (filterText == null || filterText.equals("")) {
             return true;
         }
-        
-        if(value == null) {
+
+        if (value == null) {
             return false;
         }
-        
-        return ((Comparable) value).compareTo(Integer.valueOf(filterText)) > 0;
+
+        return ((Comparable) value).compareTo(getInteger(filterText)) > 0;
     }
-    
+
+    public boolean globalFilterFunction(Object value, Object filter, Locale locale) {
+        String filterText = (filter == null) ? null : filter.toString().trim().toLowerCase();
+        if (filterText == null || filterText.equals("")) {
+            return true;
+        }
+        int filterInt = getInteger(filterText);
+
+        Car car = (Car) value;
+        return car.getId().toLowerCase().contains(filterText)
+                || car.getBrand().toLowerCase().contains(filterText)
+                || car.getColor().toLowerCase().contains(filterText)
+                || (car.isSold() ? "sold" : "sale").contains(filterText)
+                || car.getYear() < filterInt
+                || car.getPrice() < filterInt;
+    }
+
+    private int getInteger(String string) {
+        try {
+            return Integer.valueOf(string);
+        }
+        catch (NumberFormatException ex) {
+            return 0;
+        }
+    }
+
     public List<String> getBrands() {
         return service.getBrands();
     }
-    
+
     public List<String> getColors() {
         return service.getColors();
     }
-    
-    public List<Car> getCars() {
-        return cars;
+
+    public List<Car> getCars1() {
+        return cars1;
     }
 
-    public List<Car> getFilteredCars() {
-        return filteredCars;
+    public List<Car> getCars2() {
+        return cars2;
     }
 
-    public void setFilteredCars(List<Car> filteredCars) {
-        this.filteredCars = filteredCars;
+    public List<Car> getFilteredCars1() {
+        return filteredCars1;
+    }
+
+    public void setFilteredCars1(List<Car> filteredCars1) {
+        this.filteredCars1 = filteredCars1;
+    }
+
+    public List<Car> getFilteredCars2() {
+        return filteredCars2;
+    }
+
+    public void setFilteredCars2(List<Car> filteredCars2) {
+        this.filteredCars2 = filteredCars2;
     }
 
     public void setService(CarService service) {
