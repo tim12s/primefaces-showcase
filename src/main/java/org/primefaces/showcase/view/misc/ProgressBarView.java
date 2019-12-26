beaba9d15f89bf1516d61b4c6ed9ac9a4acdf772
Betlista <Betlista+github@gmail.com>
2019-12-26 14:17:50 +0100

    new progressBar usage added with progress updated out of getProgress method (#106)
    * new progressBar usage added with progress updated out of getProgress method
    
    * added comments to code to inform reader about different behavior of buttons

--- src/main/java/org/primefaces/showcase/view/misc/ProgressBarView.java
+++ src/main/java/org/primefaces/showcase/view/misc/ProgressBarView.java
@@ -26,31 +26,55 @@ import java.io.Serializable;
 @ViewScoped
 public class ProgressBarView implements Serializable {
     
-    private Integer progress;
+    private Integer progress1;
+    private Integer progress2;
 
-	public Integer getProgress() {
-		if(progress == null) {
-			progress = 0;
+    public Integer getProgress1() {
+        progress1 = updateProgress(progress1);
+        return progress1;
+    }
+
+    public Integer getProgress2() {
+        progress2 = updateProgress(progress2);
+        return progress2;
+    }
+
+    public void longRunning() throws InterruptedException {
+        progress2 = 0;
+        while (progress2 == null || progress2 < 100) {
+            progress2 = updateProgress(progress2);
+            Thread.sleep(500);
         }
-		else {
-			progress = progress + (int)(Math.random() * 35);
-			
-			if(progress > 100)
-				progress = 100;
-		}
-		
-		return progress;
-	}
+    }
 
-	public void setProgress(Integer progress) {
-		this.progress = progress;
-	}
-	
-	public void onComplete() {
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Progress Completed"));
-	}
-    
+    private Integer updateProgress(Integer progress) {
+        if(progress == null) {
+            progress = 0;
+        }
+        else {
+            progress = progress + (int)(Math.random() * 35);
+            
+            if(progress > 100)
+                progress = 100;
+        }
+        
+        return progress;
+    }
+
+    public void setProgress1(Integer progress1) {
+        this.progress1 = progress1;
+    }
+
+    public void setProgress2(Integer progress2) {
+        this.progress2 = progress2;
+    }
+
+    public void onComplete() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Progress Completed"));
+    }
+
     public void cancel() {
-        progress = null;
+        progress1 = null;
+        progress2 = null;
     }
 }
